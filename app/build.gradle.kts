d44a00015123ffe0a49a708f6e20bf383a87feea
lingion <lingion@hrbeu.edu.cn>
2026-06-17 20:27:50 +0800

    release: prepare v1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.lingion.sleepy"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.3"
         vectorDrawables { useSupportLibrary = true }
         resourceConfigurations += setOf("zh", "en")
     }
